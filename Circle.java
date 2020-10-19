cc710662f560485bd200b5494a180dbd82e8fa65
Haikal Ahmad <58899364+Kal01-ai@users.noreply.github.com>
2020-10-19 17:33:45 +0800

    Circle Class
    Contains the code to draw a circle.

---
+++ Circle.java
@@ -0,0 +1,27 @@
+package gui.pbl1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
+public class Circle {
+	int x, y, width, height;
+	
+	public Circle() {}
+	
+	public Circle(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void draw(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Ellipse2D.Double circle = new Ellipse2D.Double(x, y, 10, 10);
+		
+		g2d.setColor(userChosenColor); // set color to null
+		g2d.fill(circle);
+	}
+	
+Color userChosenColor;
+}
